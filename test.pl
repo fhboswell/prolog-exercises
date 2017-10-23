0ca1cd393ccb2d9577c4a6446e78c563f0f11b4b
fhboswell <franklin.henry.boswell@gmail.com>
2017-10-22 19:42:35 -0700

    adding

--- test.pl
+++ test.pl
@@ -13,4 +13,8 @@ seperate([H|T]) :- write('list = '), write([H|T]), nl,
                    write('Head = '), write(H), nl,
                    write('Tail ='), write(T).
 
+allsame([_]).
+allsame([X,Y|Rest]) :- X=Y, allsame([Y|Rest]).
 
+sumlist([ ], 0).
+sumlist([X|Rest], Sum) :- sumlist(Rest, SR), Sum is X + SR.
